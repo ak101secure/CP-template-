690e21191b1cdcac92026d878e38a11dd53b79e0
Afzal khan <afzalkhan01765@gmail.com>
2025-07-21 03:24:48 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -108,17 +108,7 @@ int main(){
 
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    ll n;cin>>n;
-    while(n !=1){
-        cout<<n<<' ';
-        if(n%2 ==0){
-            n = n / 2 ;
-        }
-        else{
-            n = 3*n +1 ;
-        }
-    }
-    cout<<1<<endl;
+   // main code .. 
     
     return 0;
 }
